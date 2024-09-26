720f8c7f7d95d8aefa29133781b361e1da7c6d7e
Yusuf <baskhanov2025@mail.ru>
2024-09-27 01:55:14 +0300

    Yusuf sdelal

--- webpack.config.js
+++ webpack.config.js
@@ -19,6 +19,10 @@ module.exports = {
         test: /\.css$/i,
         use: ["style-loader", "css-loader"],
       },
+      { 
+        test: /.(png|jpe?g|gif)$/i, 
+        type: 'asset/resource' 
+      }, 
     ],
   },
   mode: "development",
